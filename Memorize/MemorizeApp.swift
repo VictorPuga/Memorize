2185260baaa9aa0e8d2d3b2bbffe1ba200ac25d9
VictorPuga <39507381+VictorPuga@users.noreply.github.com>
2021-01-31 11:43:04 -0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Víctor Manuel Puga Ruiz on 31/01/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
